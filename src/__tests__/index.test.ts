53a708b5f39b61f2d066822689b97a9c233cdab9
John Earle <j.earle@salesforce.com>
2020-09-28 10:58:46 -0700

    add tests for token matching

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -3,11 +3,72 @@ import { testAssistant } from '@sketch-hq/sketch-assistant-utils'
 
 import Assistant from '..'
 
-test('test assistant', async () => {
+test('border-slds-color', async () => {
   const { violations, ruleErrors } = await testAssistant(
-    resolve(__dirname, './empty.sketch'),
+    resolve(__dirname, './test.sketch'),
     Assistant,
   )
-  expect(violations[0].message).toBe('Hello world')
+  const borderColorViolations = violations.find(
+    (v) => v.ruleName === 'lightning-design-system-linter/border-slds-color',
+  )?.objects
+
+  expect(borderColorViolations?.length).toBe(1)
+  expect(borderColorViolations ? borderColorViolations[0].name : null).toBe('Bad Rectangle')
+  expect(ruleErrors).toHaveLength(0)
+})
+
+test('fill-slds-color', async () => {
+  const { violations, ruleErrors } = await testAssistant(
+    resolve(__dirname, './test.sketch'),
+    Assistant,
+  )
+  const fillColorViolations = violations.find(
+    (v) => v.ruleName === 'lightning-design-system-linter/fill-slds-color',
+  )?.objects
+
+  expect(fillColorViolations?.length).toBe(1)
+  expect(fillColorViolations ? fillColorViolations[0].name : null).toBe('Bad Rectangle')
+  expect(ruleErrors).toHaveLength(0)
+})
+
+test('text-slds-color', async () => {
+  const { violations, ruleErrors } = await testAssistant(
+    resolve(__dirname, './test.sketch'),
+    Assistant,
+  )
+  const textColorViolations = violations.find(
+    (v) => v.ruleName === 'lightning-design-system-linter/text-slds-color',
+  )?.objects
+
+  expect(textColorViolations?.length).toBe(1)
+  expect(textColorViolations ? textColorViolations[0].name : null).toBe('Bad Text')
+  expect(ruleErrors).toHaveLength(0)
+})
+
+test('text-slds-font', async () => {
+  const { violations, ruleErrors } = await testAssistant(
+    resolve(__dirname, './test.sketch'),
+    Assistant,
+  )
+  const fontFamilyViolations = violations.find(
+    (v) => v.ruleName === 'lightning-design-system-linter/text-slds-font',
+  )?.objects
+
+  expect(fontFamilyViolations?.length).toBe(1)
+  expect(fontFamilyViolations ? fontFamilyViolations[0].name : null).toBe('Bad Text')
+  expect(ruleErrors).toHaveLength(0)
+})
+
+test('text-slds-size', async () => {
+  const { violations, ruleErrors } = await testAssistant(
+    resolve(__dirname, './test.sketch'),
+    Assistant,
+  )
+  const fontSizeViolations = violations.find(
+    (v) => v.ruleName === 'lightning-design-system-linter/text-slds-size',
+  )?.objects
+
+  expect(fontSizeViolations?.length).toBe(1)
+  expect(fontSizeViolations ? fontSizeViolations[0].name : null).toBe('Bad Text')
   expect(ruleErrors).toHaveLength(0)
 })
